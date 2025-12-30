6f80da2eeeabc4795b49ed625f1107bad64c279f
hienminhnguyen2003 <nguyenminhhiendng.vn@gmail.com>
2025-12-29 20:58:32 -0500

    Add files via upload

---
+++ The Big Book of Small Python Projects/Project1.py
@@ -0,0 +1,67 @@
+import random
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+import random
+def main():
+    print('''\nBagels, a deductive logic game.
+By Al Sweigart al@inventwithpython.com
+
+I am thinking of a {}-digit number with no repeated digits.
+Try to guess what it is. Here are some clues:
+When I say:    That means:
+    Pico         One digit is correct but in the wrong position.
+    Fermi        One digit is correct and in the right position.
+    Bagels       No digit is correct.
+
+For example, if the secret number was 248 and your guess was 843, the
+clues would be Fermi Pico.'''.format(NUM_DIGITS))
+    while True:
+        secretNum = getSecretNum()
+        print("I have thought up a number.")
+        print('You have {} guesses to get it.'.format(MAX_GUESSES))
+        numGuesses = 1
+        while numGuesses <= MAX_GUESSES:
+            guess = ''
+            while len(guess) != NUM_DIGITS or not guess.isdecimal():
+                print('Guess #{}: '.format(numGuesses))
+                guess = input('>')
+
+            clues = getClues(guess, secretNum)
+            print(clues)
+            if guess == secretNum:
+                    break
+            numGuesses += 1
+
+            if numGuesses>MAX_GUESSES:
+                print('You run out of guesses')
+                print('The answer was {}.'.format(secretNum))
+                print('Do you want to play again? (y/n)')
+                if not input('> ').lower().startswith('y'):
+                    print('Thanks for playing!')
+                    break
+
+def getSecretNum():
+    numbers = list('0123456789')
+    random.shuffle(numbers)
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum
+
+def getClues(guess, secretNum):
+    if guess == secretNum:
+        return 'You got it'
+    clues = []
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+             clues.append('Fermi')
+        elif guess[i] in secretNum:
+             clues.append('Pico')
+    if not clues:
+         return 'Bagels'
+    else:
+         clues.sort()
+         return ' '.join(clues)
+
+if __name__ == '__main__':
+     main()
